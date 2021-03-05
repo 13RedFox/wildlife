2661db3c5dcbb9d6a4962c21eb3d5fbabc5bc1e5
13redfox <sergei181989@gmail.com>
2021-03-05 18:29:16 +0200

    fix: fix header nav & social svg icons

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,14 @@
+const header = document.querySelector('.header');
 
+window.addEventListener('scroll', function () {
+    let scrollPos = window.scrollY;
+
+    if (scrollPos > 0) {
+        header.classList.add('transparent');
+        header.style.top = '0';
+    } else {
+        header.classList.remove('transparent');
+        header.style.top = '2rem';
+    }
+
+});

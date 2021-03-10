6fa0646d0da39bdf30e95a4f04148869453705e3
13redfox <sergei181989@gmail.com>
2021-03-10 13:57:43 +0200

    feat: add slider

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,14 +1,24 @@
+'use strict';
 const header = document.querySelector('.header');
 
 window.addEventListener('scroll', function () {
-    let scrollPos = window.scrollY;
+	let scrollPos = window.scrollY;
 
-    if (scrollPos > 0) {
-        header.classList.add('transparent');
-        header.style.top = '0';
-    } else {
-        header.classList.remove('transparent');
-        header.style.top = '2rem';
-    }
+	if (scrollPos > 0) {
+		header.classList.add('transparent');
+		header.style.top = '0';
+	} else {
+		header.classList.remove('transparent');
+		header.style.top = '2rem';
+	}
+});
 
+const swiper = new Swiper('.latest__slider', {
+	slidesPerView: 3,
+	spaceBetween: 60,
+	loop: true,
+	navigation: {
+		nextEl: '.arrow__right',
+		prevEl: '.arrow__left',
+	},
 });
